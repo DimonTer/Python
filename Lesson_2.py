cdfbbfd9f62470d937a9b7b83a052c40f00c688a
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-01-20 23:01:54 +0300

    update

---
+++ Lesson_2.py
@@ -0,0 +1,19 @@
+# type_int = 20
+# type_str = '20'
+# type_float = 20.123
+# type_bool = True
+# type_none = None  # пустота
+#
+# x = '20'
+# y = int(x)
+# print(y + 10)
+
+# x = int(input('Введите первое число: '))
+# y = int(input('Введите второе число: '))
+# z = int(input('Введите третье число: '))
+# print (x + (y * z))
+
+str1 = input('первая строка: ')
+str2 = input('вторая строка: ')
+
+print (str1 != str2)
