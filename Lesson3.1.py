fc41db6bf6257869df4641d0aa73dbf0f491c577
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-01-27 23:07:03 +0300

    update

---
+++ Lesson3.1.py
@@ -0,0 +1,8 @@
+b = 1
+while True:
+
+    a = int(input('Введите первое число   '))
+    b = int(input('Введите второе число   '))
+    if b == 0:
+        break
+    print(a / b)
---
+++ lesson3.2.py
@@ -0,0 +1,12 @@
+"""sum = 0
+while True:
+    cost = input('стоимость   ')
+    if cost == '*':
+        break
+    sum += int(cost)
+print(sum)
+"""
+
+x = int(input())
+for i in range (x):
+    print ('*')
