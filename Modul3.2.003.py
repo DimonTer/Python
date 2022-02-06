e58654e417371e8cb4d3bb205fb562531d64b1e1
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-02-06 10:05:45 +0300

    home_work3.1

---
+++ Modul3.2.003.py
@@ -0,0 +1,12 @@
+x = 0
+while True:
+    x = int(input ('Введите число '))
+    if x == 7:
+        break
+    elif x > 0:
+        print ('Number is positive')
+    elif x < 0:
+        print ('Number is negative')
+    elif x == 0:
+        print ('Number is equal to zero')
+print('Good bay')
