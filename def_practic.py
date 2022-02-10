07cb016f23d324b4ae591b0b5405a9a31563c316
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-02-10 22:03:08 +0300

    Practise

---
+++ def_practic.py
@@ -0,0 +1,4 @@
+print('skjhbdkwjhegckijhxhn'.upper())
+print('nxnnxJJJSnnzlkAK'.lower())
+print ('djshcjhdcjkhsdbvj sjhcjhdcjhdcv ikejdhvfjhkb vfd'.title())
+print ('kjsdckjc qjdckcj wsxsliochj'.capitalize())
