ef4de00034c90592981cc61908a858b1dee2dfda
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-02-15 22:45:46 +0300

    Lesson practic

--- def_practic.py
+++ def_practic.py
@@ -1,8 +1,8 @@
 y = [3, 4, 6, 77, 3, 11, 54, 3, 11, 6, 7]
 while True:
     try:
-        x = input('Введите номер ячейки  ')
-        print(y[int(x)])
+        x = int(input('Введите номер ячейки  '))
+        print(y[x])
     except ValueError:
         print('Вы ввели не число')
     except IndexError:
