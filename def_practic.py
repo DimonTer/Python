625ba2269980bc9343cc767d52bd911eff8f32a1
Dima Kovalenko <mr.kovalenko.d@gmail.com>
2022-02-17 16:49:05 +0300

    Ref return

--- def_practic.py
+++ def_practic.py
@@ -1,9 +1,9 @@
-y = [3, 4, 6, 77, 3, 11, 54, 3, 11, 6, 7]
-while True:
-    try:
-        x = int(input('Введите номер ячейки  '))
-        print(y[x])
-    except ValueError:
-        print('Вы ввели не число')
-    except IndexError:
-        print('Вы ввели не корректный  номер ячейки')
+def fuc(x):
+    return pr
+
+
+pr = 0
+x = 10
+for i in range(x):
+    pr *= (x * i)
+    print(i, pr)
